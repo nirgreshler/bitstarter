d5a7ee86c073cb33a467948c48faaefaf5dc9d62
Nir Greshler <nirgreshler@gmail.com>
2013-07-12 08:13:34 +0000

    web

--- web.js
+++ web.js
@@ -1,13 +1,13 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var fs = require('fs');
 
 app.get('/', function(request, response) {
-    var fs = require('fs');
     fs.readFile('index.html', function (err, data) {
 	if (err) throw err;
-    });
     response.send(data);
+});
 });
 
 var port = process.env.PORT || 5000;
